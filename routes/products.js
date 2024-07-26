91c984b6a516d73220403fedf3ceae4bd5159872
Hanssz <hanifamrinrasyada@gmail.com>
2024-07-26 23:10:26 +0700

    memperbarui handdle error pada file

--- routes/products.js
+++ routes/products.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const Products = require("../models/products");
 const { authenticate, authorize } = require("../middleware/auth");
-const upload = require("../middleware/upload");
 
 router.post(
   "/",
